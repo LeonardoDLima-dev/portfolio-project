89061d248b2bc02129d75a3896a0afcc004d80d2
LeonardoDLima-dev <leonardo.lima900@hotmail.com>
2025-01-09 15:49:44 -0300

    atualizacao 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import styles from './App.module.css'
 import { Navbar } from './componets/Navbar/Navbar'
+import { Hero } from './Hero/Hero'
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Hero/>
       </div>
   )
 }
