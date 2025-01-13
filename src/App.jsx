b1910111060348aafe315ec49a6397e5ac9beacf
LeonardoDLima-dev <leonardo.lima900@hotmail.com>
2025-01-13 09:42:42 -0300

    atualizacao 5

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import styles from './App.module.css'
 import { Navbar } from './componets/Navbar/Navbar'
 import { Hero } from './componets/Hero/Hero'
 import { About } from './componets/About/About'
+import { Experience } from './componets/Experience/Experience'
+
 
 function App() {
+
   return (
     <div className={styles.App}>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <About/>
-      </div>
+      <Experience/>
+    </div>
   )
 }
 
--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -1,19 +1,28 @@
-import React, {useState} from "react";
-import styles from "../Hero/hero.module.css";
+import React, { useState } from "react";
+import styles from "../Hero/Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 
 export const Hero = () => {
-    return <section className={styles.container}>
-        <div className={styles.container}>
-            <h1 className={styles.title}>Hi, i'm Leonardo</h1>
-            <p className={styles.description}>I'm a Full-Stack Developer with 2 years of experience using React and 
-                NodeJs. Reach out if you'd like to learn more! 
-            </p>
-            <a href="mailto:leonardo.lima900@hotmail.com" className={styles.contactBtn}>Contact Me</a>
+    return (
+      <section className={styles.container}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Hi, I'm Leonardo</h1>
+          <p className={styles.description}>
+            I'm a full-stack developer with 2 years of experience using React and
+            NodeJS. Reach out if you'd like to learn more!
+          </p>
+          <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+            Contact Me
+          </a>
         </div>
-        <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
-        <div className={styles.topBlur}/>
-        <div className={styles.bottomBlur}/>
-    </section>;
-}
+        <img
+          src={getImageUrl("hero/heroImage.png")}
+          alt="Hero image of me"
+          className={styles.heroImg}
+        />
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+      </section>
+    );
+  };
